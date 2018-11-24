dd0da07c8aaa8503ba96cd976a5f939b75956bc3
Nitai Charan <nitaicharan@gmail.com>
2018-11-24 09:25:48 -0300

    Update

--- src/main/java/br/unipe/jacademy/resources/SalaResource.java
+++ src/main/java/br/unipe/jacademy/resources/SalaResource.java
@@ -9,10 +9,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Map;
 import java.util.Optional;
 
 @Controller
-public class SalaResource {
+public class SalaResource <T extends Object>{
 
     @Autowired
     private SalaService service;
@@ -20,43 +21,42 @@ public class SalaResource {
     @Autowired
     private EnderecoService enderecoService;
 
+    private ModelAndView modelAndView;
+    private Map<String, T> atributo;
+
     @RequestMapping(method = RequestMethod.GET, value = "/salas")
     public ModelAndView inicio() {
-        return salas();
+        return salas("cadastro/salas");
     }
 
-    public ModelAndView salas(){
-        ModelAndView modelAndView = new ModelAndView("cadastro/salas");
-        return modelAndView.addObject("salas", service.getAll()).addObject("sala", new SalaEntity());
-    }
 
     @RequestMapping(method = RequestMethod.POST, value = "**/salvar")
-    public ModelAndView salvar(SalaEntity entity){
+    public ModelAndView salvar(SalaEntity entity) {
         entity = service.salvar(entity);
         return salas();
     }
 
-    @GetMapping("/editarsalas/{idsala}")
-    public ModelAndView editar (@PathVariable("idsala") Long idsala){
-        ModelAndView modelAndView = new ModelAndView("cadastro/salas");
+    @GetMapping("/sala/editar/{idsala}")
+    public ModelAndView editar(@PathVariable("idsala") Long idsala) {
+        ModelAndView modelAndView = new ModelAndView("sala/editar");
         Optional<SalaEntity> optional = service.getPorId(idsala);
         return modelAndView.addObject("sala", optional.get());
     }
 
-    @GetMapping("/excluir/{idsala}")
-    public ModelAndView excluir (@PathVariable("idsala") Long idsala){
+    @GetMapping("/sala/excluir/{idsala}")
+    public ModelAndView excluir(@PathVariable("idsala") Long idsala) {
         service.excluiPorId(idsala);
         return salas();
     }
 
     @PostMapping("**/pesquisarsalas")
-    public ModelAndView pesquisar(@RequestParam("nomepesquisa") String nomepesquisa){
+    public ModelAndView pesquisar(@RequestParam("nomepesquisa") String nomepesquisa) {
         ModelAndView modelAndView = new ModelAndView("cadastro/salas");
         return modelAndView.addObject("salas", service.getPorNome(nomepesquisa)).addObject("sala", new SalaEntity());
     }
 
     @GetMapping("/endereco/{idsala}")
-    public ModelAndView cadeiras (@PathVariable("idsala") Long idsala) {
+    public ModelAndView cadeiras(@PathVariable("idsala") Long idsala) {
         Optional<SalaEntity> optional = service.getPorId(idsala);
         ModelAndView modelAndView = new ModelAndView("cadastro/endereco");
         modelAndView.addObject("salas", optional.get());
@@ -64,11 +64,26 @@ public class SalaResource {
     }
 
     @PostMapping("**/addendereco/{idsala}")
-    public ModelAndView addEndereco(EnderecoEntity endereco, @PathVariable("idsala") Long idsala){
+    public ModelAndView addEndereco(EnderecoEntity endereco, @PathVariable("idsala") Long idsala) {
         ModelAndView modelAndView = new ModelAndView("cadastro/endereco");
         SalaEntity sala = service.getPorId(idsala).get();
         endereco.setSala(sala);
         enderecoService.salvar(endereco);
         return modelAndView.addObject("salas", sala);
+    }
+
+    private ModelAndView salas(String path) {
+        ModelAndView modelAndView = new ModelAndView(path);
+        return modelAndView.addObject("salas", service.getAll());
+    }
+
+    private ModelAndView newSala() {
+        ModelAndView modelAndView = new ModelAndView(path);
+        return modelAndView.addObject("salas", service.getAll());
+    }
+
+    private ModelAndView prepara(String string, T atributo) {
+        this.atributo.
+        return atributo
     }
 }
