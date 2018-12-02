e5ded04343f5026c37c1dec6e738c6dc68127701
Nitai Charan <nitaicharan@gmail.com>
2018-12-02 08:52:52 -0300

    Update

---
+++ src/main/java/br/unipe/jacademy/config/InMemorySecurityConfig.java
@@ -0,0 +1,22 @@
+package br.unipe.jacademy.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class InMemorySecurityConfig {
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		
+		auth
+			.inMemoryAuthentication().withUser("user").password(encoder.encode("user")).roles("USER")
+			.and()
+			.withUser("admin").password(encoder.encode("admin")).roles("ADMIN");
+		
+	}
+}
--- src/main/java/br/unipe/jacademy/resources/IndexController.java
+++ src/main/java/br/unipe/jacademy/resources/IndexController.java
@@ -1,18 +1,19 @@
 package br.unipe.jacademy.resources;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
-    @RequestMapping("/login")
-    public String login(){
-        return "login";
-    }
-    
-    @RequestMapping("/")
-    private String index() {
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+
+    @GetMapping("/")
+    public String index() {
     	return "index";
 	}
 }
